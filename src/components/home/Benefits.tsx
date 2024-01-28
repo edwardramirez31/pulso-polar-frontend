5947865e43600c824f23c591313485f6c724412e
Edward Ramirez <edal_ramirez@hotmail.com>
2024-01-27 21:37:21 -0500

    feat: home page finished

---
+++ src/components/home/Benefits.tsx
@@ -0,0 +1,119 @@
+import React from 'react'
+
+const Benefits = () => {
+  return (
+    <div className="relative bg-white py-16 sm:py-24 lg:py-32">
+        <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+          <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">Optimiza tu salud</h2>
+          <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">Alcanza máximos niveles de bienestar</p>
+          <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">Descongela tu potencial y descubre los sorprendetes beneficios de la inmersión en hielo. Súmergete en una experiencia revitalizante qye estimula tu cuerpo y mente.</p>
+          <div className="mt-12">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+              <div className="pt-6">
+                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+</svg>
+
+                      </span>
+                    </div>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">Sistema Vascular</h3>
+                    <p className="mt-5 text-base text-gray-500">Estimula inmediatamente el flujo sanguíneo, reduciendo el ritmo cardíaco y estrés</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-6">
+                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181" />
+</svg>
+
+                      </span>
+                    </div>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">Inflamación</h3>
+                    <p className="mt-5 text-base text-gray-500">Las bajas temperaturas alivian la inflamación en el cuerpo.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-6">
+                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M10.5 8.25h3l-3 4.5h3" />
+</svg>
+
+                        
+                      </span>
+                    </div>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">Sueño Óptimo</h3>
+                    <p className="mt-5 text-base text-gray-500">Sincroniza con el ritmo circadiano y ten sueños reparadores y profundos.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-6">
+                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
+                        <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+                        </svg>
+                      </span>
+                    </div>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">Sistema Inmune</h3>
+                    <p className="mt-5 text-base text-gray-500">Fortalece el sistema inmunológico y dile adios a la enfermedad.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-6">
+                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
+                        <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                      </span>
+                    </div>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">Metabolismo</h3>
+                    <p className="mt-5 text-base text-gray-500">Eleva tus niveles de atención y enfoque cognitivo para un alto rendimiento.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-6">
+                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
+                      <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                        </svg>
+                      </span>
+                    </div>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">Resiliencia</h3>
+                    <p className="mt-5 text-base text-gray-500">Entrena la mente para aceptar el entorno y manejar el estrés.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Benefits
---
+++ src/components/home/Blog.tsx
@@ -0,0 +1,120 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div className="relative bg-gray-50 py-16 sm:py-24 lg:py-32">
+        <div className="relative">
+          <div className="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+            <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">Aprende</h2>
+            <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">Últimas Tendencias</p>
+            <p className="mt-5 mx-auto max-w-prose text-xl text-gray-500">Mantente informado de los últimos eventos de la comunidad, experiencias, estudios científicos, guías de desarrollo integral y mucho más.</p>
+          </div>
+          <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
+            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+              <div className="flex-shrink-0">
+                <img className="h-48 w-full object-cover" src="/icebath5.png" alt=""/>
+              </div>
+              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1">
+                  <p className="text-sm font-medium text-cyan-600">
+                    <a href="#" className="hover:underline"> Artículo </a>
+                  </p>
+                  <a href="#" className="block mt-2">
+                    <p className="text-xl font-semibold text-gray-900">Baños de hielo ¿Antes o después de entreno?</p>
+                    <p className="mt-3 text-base text-gray-500">Dependiendo de tus objetivos y necesidades, los baños de hielo antes del entreno favorecerán el metabolismo y después del entrenamiento reducen la inflamación y desestimula ciertas hormonas.</p>
+                  </a>
+                </div>
+                {/* <div className="mt-6 flex items-center">
+                  <div className="flex-shrink-0">
+                    <a href="#">
+                      <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Roel Aufderehar"/>
+                    </a>
+                  </div>
+                  <div className="ml-3">
+                    <p className="text-sm font-medium text-gray-900">
+                      <a href="#" className="hover:underline"> Roel Aufderehar </a>
+                    </p>
+                    <div className="flex space-x-1 text-sm text-gray-500">
+                      <time> Mar 16, 2020 </time>
+                      <span aria-hidden="true"> &middot; </span>
+                      <span> 6 min read </span>
+                    </div>
+                  </div>
+                </div> */}
+              </div>
+            </div>
+
+            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+              <div className="flex-shrink-0">
+                <img className="h-48 w-full object-cover" src="/icebath6.png" alt=""/>
+              </div>
+              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1">
+                  <p className="text-sm font-medium text-cyan-600">
+                    <a href="#" className="hover:underline"> Video </a>
+                  </p>
+                  <a href="#" className="block mt-2">
+                    <p className="text-xl font-semibold text-gray-900">¿Cómo reacciona tu cuerpo al frío extremo?</p>
+                    <p className="mt-3 text-base text-gray-500">Someter el cuerpo a frío extremo en períodos de tiempo cortos y controlados estimula ciertos sistemas y procesos fisiólogicos que resultan en un estrés hormético con numerosos beneficios para la salud.</p>
+                  </a>
+                </div>
+                {/* <div className="mt-6 flex items-center">
+                  <div className="flex-shrink-0">
+                    <a href="#">
+                      <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Brenna Goyette"/>
+                    </a>
+                  </div>
+                  <div className="ml-3">
+                    <p className="text-sm font-medium text-gray-900">
+                      <a href="#" className="hover:underline"> Brenna Goyette </a>
+                    </p>
+                    <div className="flex space-x-1 text-sm text-gray-500">
+                      <time> Mar 10, 2020 </time>
+                      <span aria-hidden="true"> &middot; </span>
+                      <span> 4 min read </span>
+                    </div>
+                  </div>
+                </div> */}
+              </div>
+            </div>
+
+            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+              <div className="flex-shrink-0">
+                <img className="h-48 w-full object-cover" src="/icebath2.png" alt=""/>
+              </div>
+              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1">
+                  <p className="text-sm font-medium text-cyan-600">
+                    <a href="#" className="hover:underline"> Experiencia </a>
+                  </p>
+                  <a href="#" className="block mt-2">
+                    <p className="text-xl font-semibold text-gray-900">Reserva tu lugar en la expedición helada a Santurbán</p>
+                    <p className="mt-3 text-base text-gray-500">Únete a la comunidad y vive la experiencia de frío, respiración y entrenamiento mental avanzada este mes de febrero. Serán tres días y dos noches en las que conoceremos nuestro cuerpo, mente y su capacidad como nunca.</p>
+                  </a>
+                </div>
+                {/* <div className="mt-6 flex items-center">
+                  <div className="flex-shrink-0">
+                    <a href="#">
+                      <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="Daniela Metz"/>
+                    </a>
+                  </div>
+                  <div className="ml-3">
+                    <p className="text-sm font-medium text-gray-900">
+                      <a href="#" className="hover:underline"> Daniela Metz </a>
+                    </p>
+                    <div className="flex space-x-1 text-sm text-gray-500">
+                      <time> Feb 12, 2020 </time>
+                      <span aria-hidden="true"> &middot; </span>
+                      <span> 11 min read </span>
+                    </div>
+                  </div>
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Blog
